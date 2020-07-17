eb95c9a6374a3737ef7ce6ca64a2d7ba21ba293f
Anna Buczek <aania.buczek@gmail.com>
2020-07-17 11:40:48 +0200

    Refactor warriors action creators

--- src/api/warriorsApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL:
-    'https://run.mocky.io/v3/9fbe35dc-2333-454d-a3a3-2951ef978db1',
-});
--- src/redux/warriors/actions.js
+++ src/redux/warriors/actions.js
@@ -1,28 +1,47 @@
 import _ from 'lodash';
 import actionTypes from '../actionTypes';
-import api from '../../api/warriorsApi';
+import axios from 'axios';
 import { setLocalStorageUpdateDate } from '../localStorage/actions';
 
-export const fetchWarriors = () => {
-  return (dispatch, getState) => {
-    dispatch({ type: actionTypes.FETCH_WARRIORS_REQUESTED });
-    return api
-      .get('/')
-      .then(({ data: { warriors } }) => {
-        dispatch({
-          type: actionTypes.FETCH_WARRIORS_SUCCEEDED,
-          data: {
-            ...getState().warriors.data,
-            ..._.mapKeys(warriors, 'id'),
-          },
-        });
-        dispatch(setLocalStorageUpdateDate());
-      })
-      .catch((error) => {
-        dispatch({
-          type: actionTypes.FETCH_WARRIORS_FAILED,
-          error,
-        });
-      });
+export const fetchWarriorsRequested = () => {
+  return {
+    type: actionTypes.FETCH_WARRIORS_REQUESTED,
   };
+};
+
+export const fetchWarriorsSucceeded = (
+  currentWarriors,
+  fetchedWarriors,
+) => {
+  return {
+    type: actionTypes.FETCH_WARRIORS_SUCCEEDED,
+    data: {
+      ...currentWarriors,
+      ..._.mapKeys(fetchedWarriors, 'id'),
+    },
+  };
+};
+
+export const fetchWarriorsFailed = (error) => {
+  return {
+    type: actionTypes.FETCH_WARRIORS_FAILED,
+    error,
+  };
+};
+
+export const fetchWarriors = () => async (dispatch, getState) => {
+  dispatch(fetchWarriorsRequested());
+  try {
+    const {
+      data: { warriors },
+    } = await axios.get(
+      'https://run.mocky.io/v3/9fbe35dc-2333-454d-a3a3-2951ef978db1/',
+    );
+    dispatch(
+      fetchWarriorsSucceeded(getState().warriors.data, warriors),
+    );
+    dispatch(setLocalStorageUpdateDate());
+  } catch (error) {
+    dispatch(fetchWarriorsFailed(error.message));
+  }
 };
