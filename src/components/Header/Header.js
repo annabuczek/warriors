3a361f55599a1ceb4f605972004e1d275e84be54
Anna Buczek <aania.buczek@gmail.com>
2020-07-18 17:30:11 +0200

    Add and style mobile navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
+import './Hamburger.scss';
+
+const NavigationLinks = ({ mobile, handleClick }) => {
+  return (
+    <div className="header__links">
+      <Link
+        to="/mylist"
+        className={`header__link ${
+          mobile ? 'header__link--mobile' : ''
+        }`}
+        onClick={handleClick}
+      >
+        Moja Lista
+      </Link>
+      <Link
+        to="/warriors/new"
+        className="header__link header__link--add"
+        onClick={handleClick}
+      >
+        Dodaj Wojownika
+      </Link>
+    </div>
+  );
+};
+
+const NavigationMobile = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="nav-mobile">
+      <button
+        className={`hamburger hamburger--spin ${
+          open ? 'is-active' : ''
+        }`}
+        type="button"
+        onClick={(e) => setOpen(!open)}
+      >
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+      <div
+        className={`nav-mobile__drawer ${
+          open ? 'nav-mobile__drawer--open' : ''
+        }`}
+      >
+        <NavigationLinks mobile handleClick={() => setOpen(false)} />
+      </div>
+    </div>
+  );
+};
+
+const NavigationDesktop = () => {
+  return (
+    <div className="nav-desktop">
+      <NavigationLinks />
+    </div>
+  );
+};
 
 const Header = () => {
   return (
     <section className="header">
-      <nav className="header__navigation">
-        <div className="header__navigation--left">
-          <Link to="/" className="header__link">
-            Strona Główna
-          </Link>
-        </div>
-        <div className="header__navigation--right">
-          <Link to="/mylist" className="header__link">
-            Moja Lista
-          </Link>
-          <Link to="/warriors/new" className="header__link">
-            Dodaj Wojownika
-          </Link>
-        </div>
+      <nav className="header__navbar">
+        <Link to="/" className="header__link">
+          Strona Główna
+        </Link>
+        <NavigationMobile />
+        <NavigationDesktop />
       </nav>
     </section>
   );
