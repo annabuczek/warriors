008468c6562db58039db3e59758b5f175917e12a
Anna Buczek <aania.buczek@gmail.com>
2020-07-19 18:00:54 +0200

    Fix my list action creators tests

--- src/redux/myList/actions.test.js
+++ src/redux/myList/actions.test.js
@@ -7,21 +7,27 @@ import {
 describe('myList actions', () => {
   describe('addWarriorToMyList', () => {
     it('creates ADD_WARRIOR_TO_MY_LIST action', () => {
+      const warrior = { id: 1, name: 'Wojowniczka Kasia' };
       const expectedAction = {
         type: actionTypes.ADD_WARRIOR_TO_MY_LIST,
+        warrior,
       };
 
-      expect(addWarriorToMyList()).toEqual(expectedAction);
+      expect(addWarriorToMyList(warrior)).toEqual(expectedAction);
     });
   });
 
   describe('removeWarriorFromMyList', () => {
     it('creates REMOVE_WARRIOR_FROM_MY_LIST action', () => {
+      const warrior = { id: 1, name: 'Wojowniczka Jasia' };
       const expectedAction = {
         type: actionTypes.REMOVE_WARRIOR_FROM_MY_LIST,
+        warrior,
       };
 
-      expect(removeWarriorFromMyList()).toEqual(expectedAction);
+      expect(removeWarriorFromMyList(warrior)).toEqual(
+        expectedAction,
+      );
     });
   });
 });
